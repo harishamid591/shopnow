760921032166ecb5dfe52ca18a64d34a7aa44641
harishamid591 <harishamid591@gmail.com>
2025-06-28 11:58:52 +0530

    fix puppeteer

--- controllers/user/orderController.js
+++ controllers/user/orderController.js
@@ -504,7 +504,11 @@ const generateInvoice = async (req, res) => {
     const html = await ejs.renderFile(invoiceTemplatePath, { order });
 
     // Launch Puppeteer and generate PDF
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'networkidle0' });
 
